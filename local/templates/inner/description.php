146fb6b3eab4f125ff9a7cee559019c4e6046ff8
Amadare <amxdxre@gmail.com>
2023-09-12 02:22:41 +0300

    Added description to site template.

---
+++ local/templates/inner/description.php
@@ -0,0 +1,5 @@
+<?php
+$arTemplate = Array(
+	"NAME"=>GetMessage('INNER_TEMPLATE_NAME'),
+	"DESCRIPTION"=>GetMessage('INNER_TEMPLATE_DESC')
+);
---
+++ local/templates/inner/lang/ru/description.php
@@ -0,0 +1,3 @@
+<?php
+$MESS ['INNER_TEMPLATE_NAME']="Внутренний шаблон";
+$MESS ['INNER_TEMPLATE_DESC']="Внутренний шаблон: первая академия";

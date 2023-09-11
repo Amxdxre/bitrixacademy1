594555ee14ba12251c6a4b92ae65374818cb83f7
Amadare <amxdxre@gmail.com>
2023-09-12 02:23:37 +0300

    Refactored php_interface functions.

--- bitrix/php_interface/init.php
+++
@@ -1,4 +0,0 @@
-<?
-if (file_exists($_SERVER["DOCUMENT_ROOT"]. "/bitrix/php_interface/include/functions.php"))
-    require_once ($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/include/functions.php");
-?>

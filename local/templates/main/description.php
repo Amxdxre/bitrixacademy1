6b3fdfcd863377536b043dd029aaf990ca0035f6
Amadare <amxdxre@gmail.com>
2023-09-12 02:33:21 +0300

    Added template for main page.

---
+++ local/templates/main/description.php
@@ -0,0 +1,5 @@
+<?php
+$arTemplate = array(
+    "NAME"=>GetMessage('MAIN_TEMPLATE_NAME'),
+    "DESCRIPTION"=>GetMessage('MAIN_TEMPLATE_DESC')
+);
---
+++ local/templates/main/lang/ru/description.php
@@ -0,0 +1,3 @@
+<?php
+$MESS ['MAIN_TEMPLATE_NAME']="Основной шаблон";
+$MESS ['MAIN_TEMPLATE_DESC']="Основной шаблон: первая академия";

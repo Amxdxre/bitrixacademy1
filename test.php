b6c4a020d1e091891bb7718418e849c5fc4e1b3b
Amadare <amxdxre@gmail.com>
2023-11-13 18:36:58 +0300

    Deleted menu component in test page.

--- test.php
+++ test.php
@@ -1,32 +1,4 @@
-<?php
-/** @var array $arParams */
-/** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
-$APPLICATION->SetTitle("Тестовая страница");
-?><?php $APPLICATION->IncludeComponent(
-    "bitrix:menu",
-    "horizontal_multilevel_test",
-    array(
-        "ALLOW_MULTI_SELECT" => "N",
-        "CHILD_MENU_TYPE" => "left",
-        "DELAY" => "N",
-        "MAX_LEVEL" => "1",
-        "MENU_CACHE_GET_VARS" => array(),
-        "MENU_CACHE_TIME" => "3600",
-        "MENU_CACHE_TYPE" => "N",
-        "MENU_CACHE_USE_GROUPS" => "Y",
-        "ROOT_MENU_TYPE" => "left",
-        "USE_EXT" => "N",
-        "COMPONENT_TEMPLATE" => "horizontal_multilevel_test"
-    ),
-    false
-); ?><?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Тестовая Страница");
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>

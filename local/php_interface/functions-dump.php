ad137b1708298171a8738a4c5ad2d24b68a0c52d
Amadare <amxdxre@gmail.com>
2023-11-15 01:30:31 +0300

    Задание 4.2 - Пункт 5. Доработана функция отладки для пользователей с правами администратора.

--- local/php_interface/functions-dump.php
+++ local/php_interface/functions-dump.php
@@ -2,7 +2,7 @@
 function test_dump($var, $die = false, $all = false)
 {
     global $USER;
-    if (($USER->GetID() == 1) || ($all == true)) {
+    if (($USER->IsAdmin()) || ($all == true)) {
         ?>
         <font style="text-align: left; font-size: 10px">
             <pre><? var_dump($var) ?></pre>

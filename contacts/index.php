2fab418c461892c5e217d0895d40d9e5bc269d9a
Amadare <amxdxre@gmail.com>
2023-11-15 02:03:29 +0300

    Задание 4.2 - Добавлен компонент обратной связи в разделе Контакты.

---
+++ contacts/index.php
@@ -0,0 +1,67 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Контакты");
+?><p>
+	 Обратитесь к нашим специалистам и получите профессиональную консультацию по вопросам создания и покупки мебели (от дизайна, разработки технического задания до доставки мебели к Вам домой).
+</p>
+<p>
+	 Вы можете обратиться к нам по телефону, по электронной почте или договориться о встрече в нашем офисе. Будем рады помочь вам и ответить на все ваши вопросы.
+</p>
+<h2>Телефоны</h2>
+<ul>
+	<li>Телефон/факс:
+	<ul>
+		<li><b>(495) 212-85-06</b></li>
+	</ul>
+ </li>
+	<li>Телефоны:
+	<ul>
+		<li><b>(495) 212-85-07</b></li>
+		<li><b>(495) 212-85-08</b></li>
+	</ul>
+ </li>
+</ul>
+<h2>Email</h2>
+<ul>
+	<li><a href="mailto:info@example.ru">info@example.ru</a> — общие вопросы</li>
+	<li><a href="mailto:sales@example.ru">sales@example.ru</a> — приобретение продукции</li>
+	<li><a href="mailto:marketing@example.ru">marketing@example.ru</a> — маркетинг/мероприятия/PR</li>
+</ul>
+<h2>Форма обратной связи</h2>
+ <?$APPLICATION->IncludeComponent(
+	"bitrix:main.feedback",
+	"",
+	Array(
+		"EMAIL_TO" => "amxdxre@gmail.com",
+		"EVENT_MESSAGE_ID" => array("7"),
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => array("NAME","EMAIL","MESSAGE"),
+		"USE_CAPTCHA" => "Y"
+	)
+);?>
+<h2>Офис в Москве</h2>
+<p>
+	 <?$APPLICATION->IncludeComponent(
+	"bitrix:map.google.view", 
+	".default", 
+	array(
+		"CONTROLS" => array(
+			0 => "SCALELINE",
+		),
+		"INIT_MAP_TYPE" => "ROADMAP",
+		"KEY" => "ABQIAAAAOSNukcWVjXaGbDo6npRDcxS1yLxjXbTnpHav15fICwCqFS-qhhSby0EyD6rK_qL4vuBSKpeCz5cOjw",
+		"MAP_DATA" => "a:4:{s:10:\"google_lat\";d:48.805406325408036;s:10:\"google_lon\";d:44.7477036338789;s:12:\"google_scale\";i:13;s:10:\"PLACEMARKS\";a:1:{i:0;a:3:{s:4:\"TEXT\";s:31:\"Это моя компания!\";s:3:\"LON\";d:44.753871021989916;s:3:\"LAT\";d:48.80748402786835;}}}",
+		"MAP_HEIGHT" => "500",
+		"MAP_ID" => "",
+		"MAP_WIDTH" => "600",
+		"OPTIONS" => array(
+			0 => "ENABLE_SCROLL_ZOOM",
+			1 => "ENABLE_DBLCLICK_ZOOM",
+			2 => "ENABLE_DRAGGING",
+		),
+		"COMPONENT_TEMPLATE" => ".default",
+		"API_KEY" => ""
+	),
+	false
+);?>
+</p><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>

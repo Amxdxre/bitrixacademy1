1d25f3bd44f139d46b4140bc75bbf7371e4338be
Amadare <amxdxre@gmail.com>
2023-11-14 05:06:23 +0300

    Task 4.1 - 3 The same parts of the template for the main and internal pages were moved into separate files and included in both templates.

---
+++ local/templates/.default/include/footer.php
@@ -0,0 +1,73 @@
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<?php
+IncludeTemplateLangFile(__FILE__);
+global $APPLICATION;
+?>
+
+<div class="ft_container">
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:menu",
+        "ft_about_menu",
+        Array(
+            "ALLOW_MULTI_SELECT" => "N",
+            "CHILD_MENU_TYPE" => "left",
+            "COMPONENT_TEMPLATE" => ".default",
+            "DELAY" => "N",
+            "MAX_LEVEL" => "1",
+            "MENU_CACHE_GET_VARS" => "",
+            "MENU_CACHE_TIME" => "3600",
+            "MENU_CACHE_TYPE" => "N",
+            "MENU_CACHE_USE_GROUPS" => "Y",
+            "ROOT_MENU_TYPE" => "bottom",
+            "USE_EXT" => "N"
+        )
+    );?>
+    <div class="ft_catalog">
+        <h4>Каталог товаров</h4>
+        <ul>
+            <li><a href="">Кухни</a></li>
+            <li><a href="">Кровати и кушетки</a></li>
+            <li><a href="">Гарнитуры</a></li>
+            <li><a href="">Тумобчки и прихожие</a></li>
+            <li><a href="">Спальни и матрасы</a></li>
+            <li><a href="">Аксессуары</a></li>
+            <li><a href="">Столы и стулья</a></li>
+            <li><a href="">Каталоги мебели</a></li>
+            <li><a href="">Раскладные диваны</a></li>
+            <li><a href="">Кресла</a></li>
+        </ul>
+
+    </div>
+    <div class="ft_contacts">
+        <h4><?php echo GetMessage('CONTACT_INFO'); ?></h4>
+        <!-- vCard        http://help.yandex.ru/webmaster/hcard.pdf      -->
+        <p class="vcard">
+						<span class="adr">
+							<span class="street-address">ул. Летняя стр.12, офис 512</span>
+						</span>
+            <span class="tel"><?$APPLICATION->IncludeComponent(
+                    "bitrix:main.include",
+                    "",
+                    Array(
+                        "AREA_FILE_RECURSIVE" => "Y",
+                        "AREA_FILE_SHOW" => "file",
+                        "AREA_FILE_SUFFIX" => "inc",
+                        "EDIT_TEMPLATE" => "",
+                        "PATH" => "/include/phone.php"
+                    )
+                );?></span>
+            <strong><?php echo GetMessage('WORKING_TIME'); ?></strong> <br/> <span class="workhours">ежедневно с 9-00 до 18-00</span><br/>
+        </p>
+        <ul class="ft_solcial">
+            <li><a href="" class="fb"></a></li>
+            <li><a href="" class="tw"></a></li>
+            <li><a href="" class="ok"></a></li>
+            <li><a href="" class="vk"></a></li>
+        </ul>
+        <div class="ft_copyright">© 2000 - 2012 "Мебельный магазин"</div>
+
+
+    </div>
+
+    <div class="clearboth"></div>
+</div>
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -25,77 +25,7 @@ global $APPLICATION;
 <?php $APPLICATION->ShowPanel(); ?>
 <div class="wrap">
     <div class="hd_header_area">
-        <div class="hd_header">
-            <table>
-                <tr>
-                    <td rowspan="2" class="hd_companyname">
-                        <h1><a href="">Мебельный магазин</a></h1>
-                    </td>
-                    <td rowspan="2" class="hd_txarea">
-                        <span class="tel"><?$APPLICATION->IncludeComponent(
-                                "bitrix:main.include",
-                                "",
-                                Array(
-                                    "AREA_FILE_RECURSIVE" => "Y",
-                                    "AREA_FILE_SHOW" => "file",
-                                    "AREA_FILE_SUFFIX" => "inc",
-                                    "EDIT_TEMPLATE" => "",
-                                    "PATH" => "/include/phone.php"
-                                )
-                            );?></span> <br/>
-                        <?php echo GetMessage('WORKING_TIME'); ?> <span
-                                class="workhours">ежедневно с 9-00 до 18-00</span>
-                    </td>
-                    <td style="width:232px">
-                        <form action="">
-                            <div class="hd_search_form" style="float:right;">
-                                <input placeholder="Поиск" type="text"/>
-                                <input type="submit" value=""/>
-                            </div>
-                        </form>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="padding-top: 11px;">
-							<span class="hd_singin"><a id="hd_singin_but_open" href="">Войти на сайт</a>
-							<div class="hd_loginform">
-								<span class="hd_title_loginform">Войти на сайт</span>
-								<form name="" method="" action="">
-
-									<input placeholder="Логин" type="text">
-									<input placeholder="Пароль" type="password">
-									<a href="/" class="hd_forgotpassword">Забыли пароль</a>
-
-									<div class="head_remember_me" style="margin-top: 10px">
-										<input id="USER_REMEMBER_frm" name="USER_REMEMBER" value="Y" type="checkbox">
-										<label for="USER_REMEMBER_frm" title="Запомнить меня на этом компьютере">Запомнить меня</label>
-									</div>
-									<input value="Войти" name="Login" style="margin-top: 20px;" type="submit">
-									</form>
-								<span class="hd_close_loginform">Закрыть</span>
-							</div>
-							</span><br>
-                        <a href="" class="hd_signup">Зарегистрироваться</a>
-                    </td>
-                </tr>
-            </table>
-            <? $APPLICATION->IncludeComponent(
-                "bitrix:menu",
-                "top_multi_menu",
-                array(
-                    "ALLOW_MULTI_SELECT" => "N",
-                    "CHILD_MENU_TYPE" => "left",
-                    "DELAY" => "N",
-                    "MAX_LEVEL" => "2",
-                    "MENU_CACHE_GET_VARS" => array(0 => "",),
-                    "MENU_CACHE_TIME" => "3600",
-                    "MENU_CACHE_TYPE" => "N",
-                    "MENU_CACHE_USE_GROUPS" => "Y",
-                    "ROOT_MENU_TYPE" => "top",
-                    "USE_EXT" => "N"
-                )
-            ); ?>
-        </div>
+        <?php include_once($_SERVER['DOCUMENT_ROOT']."/local/templates/.default/include/header.php");?>
     </div>
 
     <!--- // end header area --->

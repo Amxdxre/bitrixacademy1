adb20314b91643dcbda966f97d9fe9ed4f9c8672
Amadare <amxdxre@gmail.com>
2023-12-05 01:42:51 +0300

    Задание 10. Пункт 1. Форма поиска размещена в шапке сайта.

--- local/templates/.default/include/header.php
+++ local/templates/.default/include/header.php
@@ -26,12 +26,14 @@ global $APPLICATION;
                     class="workhours">ежедневно с 9-00 до 18-00</span>
             </td>
             <td style="width:232px">
-                <form action="">
-                    <div class="hd_search_form" style="float:right;">
-                        <input placeholder="Поиск" type="text"/>
-                        <input type="submit" value=""/>
-                    </div>
-                </form>
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:search.form",
+                    "search",
+                    Array(
+                        "PAGE" => "#SITE_DIR#search/index.php",
+                        "USE_SUGGEST" => "N"
+                    )
+                );?>
             </td>
         </tr>
         <tr>

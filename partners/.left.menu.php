56de96c0a982f1a4ec3a76fb4b6eb1f83af3c48d
Amadare <amxdxre@gmail.com>
2023-12-31 14:37:25 +0300

    Задание №2 - Созданы раздел отзывы. Создан раздел партрнёрам, созданы страницы внутри раздела.

---
+++ partners/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/partners/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partners/schedule_of_events/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером ", 
+		"/partners/how_to_become_a_partner.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>

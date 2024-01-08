5c4327dbc7d22d6fb560b9446171b0c8caa11fb1
Amadare <amxdxre@gmail.com>
2024-01-08 04:19:57 +0300

    Исправлена опечатка.

--- local/php_interface/include/functions.php
+++ local/php_interface/include/functions.php
@@ -4,9 +4,9 @@ function dump($var, $die = false, $all = false)
     global $USER;
     if ($USER->IsAdmin() || $all) {
         ?>
-        <iont style="text-align: left; font-size: 10px">
+        <font style="text-align: left; font-size: 10px">
             <pre><? var_dump($var) ?></pre>
-        </iont><br>
+        </font><br>
         <?
     }
     if ($die) {

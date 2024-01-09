b2afe93b38fd7c99d097a846b97dfcf1e0e0ed9c
Amadare <amxdxre@gmail.com>
2024-01-10 00:29:11 +0300

    Добавлен эрмитаж для отзывов.

--- local/templates/.default/components/bitrix/news.list/reviews_list/template.php
+++ local/templates/.default/components/bitrix/news.list/reviews_list/template.php
@@ -13,8 +13,14 @@
 $this->setFrameMode(true);
 ?>
 
+
+
 <? foreach ($arResult["ITEMS"] as $arItem): ?>
-    <div class="review-block">
+    <?php
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    ?>
+    <div class="review-block" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
         <div class="review-text">
 
             <div class="review-block-title"><span class="review-block-name"><a

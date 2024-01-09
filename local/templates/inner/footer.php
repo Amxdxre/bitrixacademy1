ea52d907dff84680a429e173b0991fbaac4039ef
Amadare <amxdxre@gmail.com>
2024-01-10 00:51:48 +0300

    Добавлена не достающая включаемая область.

--- local/templates/inner/footer.php
+++ local/templates/inner/footer.php
@@ -101,6 +101,19 @@ IncludeTemplateLangFile(__FILE__);
 	),
 	false
 );?>
+
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:main.include",
+        "",
+        Array(
+            "AREA_FILE_RECURSIVE" => "Y",
+            "AREA_FILE_SHOW" => "sect",
+            "AREA_FILE_SUFFIX" => "inc",
+            "EDIT_TEMPLATE" => "",
+            "PATH" => "/include/"
+        )
+    );?>
+
     <?$APPLICATION->IncludeComponent(
         "bitrix:news.list",
         "random_review",

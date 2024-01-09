5e94ecfe7db6e4c577ade6ef6262ec5b4bea7a83
Amadare <amxdxre@gmail.com>
2024-01-09 22:00:26 +0300

    Настроено отображение акций в разделе Акции.

--- local/templates/.default/components/bitrix/news/news_dir/bitrix/news.list/.default/template.php
+++ local/templates/.default/components/bitrix/news/news_dir/bitrix/news.list/.default/template.php
@@ -1,10 +1,23 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
-<?foreach($arResult["ITEMS"] as $arItem):?>
-	<div class="ps_head"><a class="ps_head_link" href="<?=$arItem["DETAIL_PAGE_URL"]?>"><h2 class="ps_head_h"><?=$arItem["NAME"]?></h2></a><span class="ps_date"><?=$arItem["DISPLAY_ACTIVE_FROM"]?></span></div>
-	<div class="ps_content">
-		<img src="<?=$arItem["DETAIL_PICTURE"]["SRC"]?>" align="left" alt="<?=$arItem["NAME"]?>"/> 
-		<?echo $arItem["PREVIEW_TEXT"];?>
-		<div style="clear:both"></div>
-	</div>
-<?endforeach;?>
+<? foreach ($arResult["ITEMS"] as $arItem): ?>
+    <?
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    ?>
+
+    <div id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+        <div class="ps_head"><a class="ps_head_link" href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><h2 class="ps_head_h"><? echo $arItem["NAME"] ?></h2></a>
+            <span class="ps_date"><? echo $arItem["DISPLAY_ACTIVE_FROM"] ?></span>
+        </div>
+        <div class="ps_content">
+            <img src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>" align="left" alt=""/>
+            <p><? echo $arItem["PREVIEW_TEXT"]; ?></p><br>
+            <div style="clear:both"></div>
+        </div>
+    </div>
+<? endforeach; ?>
+
+<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+    <br /><?=$arResult["NAV_STRING"]?>
+<?endif;?>

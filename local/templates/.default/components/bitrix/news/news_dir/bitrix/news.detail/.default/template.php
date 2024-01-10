f30cc4f114a706d30f2ff1433a1197aa8dd6e934
Amadare <amxdxre@gmail.com>
2024-01-10 18:40:22 +0300

    Исправлена постановка текста.

--- local/templates/.default/components/bitrix/news/news_dir/bitrix/news.detail/.default/template.php
+++ local/templates/.default/components/bitrix/news/news_dir/bitrix/news.detail/.default/template.php
@@ -3,5 +3,5 @@
 <?if(is_array($arResult["DETAIL_PICTURE"])):?>
 <img src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>" align="left" alt="<?=$arResult["NAME"]?>"/> 
 <?endif;?>
-<?echo $arResult["DETAIL_TEXT"];?>
+<?echo $arResult["DETAIL_TEXT"];?><br>
 

07c827bc4fecbd815263959539ccaea66a5d6981
Amadare <amxdxre@gmail.com>
2024-01-10 18:37:03 +0300

    Фикс левого меню каталога.

--- products/.left.menu_ext.php
+++ products/.left.menu_ext.php
@@ -9,8 +9,8 @@ $aMenuLinksExt = $APPLICATION->IncludeComponent(
 	array(
 		"IS_SEF" => "Y",
 		"SEF_BASE_URL" => "",
-		"SECTION_PAGE_URL" => "#SECTION_ID#/",
-		"DETAIL_PAGE_URL" => "#SECTION_ID#/#ELEMENT_ID#",
+		"SECTION_PAGE_URL" => "#CODE#/",
+		"DETAIL_PAGE_URL" => "#SECTION_ID#/#IBLOCK_CODE#",
 		"IBLOCK_TYPE" => "products",
 		"IBLOCK_ID" => "2",
 		"DEPTH_LEVEL" => "3",

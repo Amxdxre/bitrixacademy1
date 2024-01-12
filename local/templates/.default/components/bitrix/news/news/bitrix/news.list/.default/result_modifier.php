8a7e4a79fe9fbce1608ad0c85382390d89efa1e5
Amadare <amxdxre@gmail.com>
2024-01-13 02:29:04 +0300

    1.1 Создан модификатор $arResult для новостей.

--- local/templates/.default/components/bitrix/news/news/bitrix/news.list/.default/result_modifier.php
+++ local/templates/.default/components/bitrix/news/news/bitrix/news.list/.default/result_modifier.php
@@ -1,1 +1,8 @@
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<?php global $arResult; ?>
 <?php
+foreach ($arResult['ITEMS'] as $ID=>$arItems){
+    $arItems= CFile::ResizeImageGet($arItems['DETAIL_PICTURE '], array('width'=>$arParams['LIST_PREV_PICT_W'], 'height'=>$arParams['LIST_PREV_PICT_H']), BX_RESIZE_IMAGE_PROPORTIONAL, true);
+    $arResult['ITEMS'][$ID]['DETAIL_PICTURE'] = $arImage;
+}
+?>

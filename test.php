3bff3b3252ae0dae67dd304add94abc5735df139
Amadare <amxdxre@gmail.com>
2024-01-13 02:02:14 +0300

    Очищена тестовая страница.

--- test.php
+++ test.php
@@ -1,4 +1,6 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Тестовая Страница");
-?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<?php
+global $APPLICATION;
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetPageProperty("test", "Тестовое значение");
+$APPLICATION->SetTitle("Тестовая страница");
+?><?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>

f18efa6fd0fbfef50386b77f1674010c8b9146bb
Amadare <amxdxre@gmail.com>
2024-01-22 23:05:34 +0300

    1.1 Выведено в метатег description.

--- local/templates/.default/components/bitrix/news/news/bitrix/news.detail/.default/component_epilog.php
+++ local/templates/.default/components/bitrix/news/news/bitrix/news.detail/.default/component_epilog.php
@@ -1,2 +1,11 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-dump($arResult);
+
+if (is_array('AUTHOR')) {
+    $str = $arResult['NAME'] . ', ' . implode(',', $arResult['AUTHOR']) . ', ' . $arResult['DETAIL_TEXT_50'];
+} else {
+    $str = $arResult['NAME'];
+}
+
+echo $str;
+
+$APPLICATION->SetPageProperty('description', $str);

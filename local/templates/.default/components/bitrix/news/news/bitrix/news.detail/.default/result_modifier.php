3d3948fa88759dbda8e78fbc5f67d0f25e365e12
Amadare <amxdxre@gmail.com>
2024-01-22 23:03:28 +0300

    1.1 Выставлено 100 букв на вывод из детального текста.

--- local/templates/.default/components/bitrix/news/news/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/.default/components/bitrix/news/news/bitrix/news.detail/.default/result_modifier.php
@@ -1,14 +1,14 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 <?php
 $objectComponent = $this->__component;
-if ($arResult['PROPERTIES']['AUTHOR']['VALUE']){
+if ($arResult['PROPERTIES']['AUTHOR']['VALUE']) {
     $rsUser = CUser::GetByID($arResult['PROPERTIES']['AUTHOR']['VALUE']);
     $arUser = $rsUser->Fetch();
-    $arResult['AUTHOR'] = array($arUser['NAME'],$arUser['LAST_NAME']);
+    $arResult['AUTHOR'] = array($arUser['NAME'], $arUser['LAST_NAME']);
 
     $objectComponent->SetResultCacheKeys(array('AUTHOR'));
 }
 
-$arResult['DETAIL_TEXT_50']=substr($arResult['DETAIL_TEXT'],0,100).'...';
+$arResult['DETAIL_TEXT_50'] = substr($arResult['DETAIL_TEXT'], 0, 100) . '...';
 $objectComponent->SetResultCacheKeys(array('DETAIL_TEXT_50'));
 ?>
